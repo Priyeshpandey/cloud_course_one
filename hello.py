ee40f9804f4f64b8c2d5a26e4b422ee3acfb0268
Priyesh Pandey <pandeypriyesh184@gmail.com>
2022-01-11 13:45:42 +0000

    changes from azure

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ def add(a, b):
     return a + b
 
 
+def sub(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
     x, y = 1, 2
     print(f"Result for sum of {x} + {y} = {add(x,y)}")
--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,13 @@
-from hello import add
+from hello import add, sub
 
 
 def test_add():
     exp_res = 8
     calc_res = add(1, 7)
+    assert calc_res == exp_res, f"Expected {exp_res}, got {calc_res}"
+
+
+def test_sub():
+    exp_res = -6
+    calc_res = sub(1, 7)
     assert calc_res == exp_res, f"Expected {exp_res}, got {calc_res}"
