c6c9930e1191af3f9c3081bec1d4d06fa75c6457
EC2 Default User <ec2-user@ip-172-31-85-53.ec2.internal>
2022-01-11 00:53:57 +0000

    created basic structure

---
+++ hello.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    x, y = 1, 2
+    print(f"Result for sum of {x} + {y} = {add(x,y)}")
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+from hello import add
+
+
+def test_add():
+    exp_res = 8
+    calc_res = add(1, 7)
+    assert calc_res == exp_res, f"Expected {exp_res}, got {calc_res}"
